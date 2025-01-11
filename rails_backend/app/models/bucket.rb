e2f32e09ec70076494d7c61c87863256c96c1295
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2025-01-11 13:58:40 +0500

    add relationships and validations to the models - rails
    previously Task model has: belongs_to :bucket and other was empty

--- rails_backend/app/models/bucket.rb
+++ rails_backend/app/models/bucket.rb
@@ -1,2 +1,6 @@
 class Bucket < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true
+  validates :status, inclusion: { in: %w[Empty Pending Completed] }
 end
--- rails_backend/app/models/task.rb
+++ rails_backend/app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :bucket
+
+  validates :name, presence: true
+  validates :status, inclusion: { in: %w[Pending Completed] }
 end
